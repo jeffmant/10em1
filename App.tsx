2f610c859587e0d4bafadc063b5910fec9d007fe
Jefferson Mantovani <jgsmantovani@gmail.com>
2023-12-27 09:58:56 -0300

    feat: add font

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import {  
+  useFonts, 
+  Nunito_300Light, 
+  Nunito_400Regular, 
+  Nunito_500Medium, 
+  Nunito_600SemiBold, 
+  Nunito_700Bold 
+} from '@expo-google-fonts/nunito';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_300Light, 
+    Nunito_400Regular, 
+    Nunito_500Medium, 
+    Nunito_600SemiBold, 
+    Nunito_700Bold 
+  });
+
   return (
-    <Text>
-      Hello
-    </Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {
+        fontsLoaded && 
+          <Text style={{ fontFamily: 'Nunito_400Regular', fontSize: 40 }}>10em1</Text>
+      }
+    </View>
   );
 }
