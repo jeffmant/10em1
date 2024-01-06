f86412fcaddbfe6d21d6af3e56e1d6286aa9f227
Jefferson Mantovani <jgsmantovani@gmail.com>
2024-01-06 10:29:58 -0300

    style: add background at auth pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,24 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { Box, useTheme } from "native-base";
 import { AppRoutes } from "./app.routes";
-import { SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { SignedIn, SignedOut, useSession } from "@clerk/clerk-expo";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes () {
   const { colors } = useTheme()
+  const { isSignedIn } = useSession()
 
   const theme = DefaultTheme;
-  theme.colors.background= colors.gray['200']
+  theme.colors.background = isSignedIn ? colors.gray['200'] : "#2a4858"
 
   return (
-    <Box flex={1} bg="gray.200">
+    <Box flex={1} bg={ isSignedIn ? "gray.200" : "#2a4858"}>
       <NavigationContainer theme={theme}>
         <SignedIn>
-          <AppRoutes />
+            <AppRoutes />
         </SignedIn>
         <SignedOut>
-          <AuthRoutes />
+            <AuthRoutes />
         </SignedOut>
       </NavigationContainer>
     </Box>
